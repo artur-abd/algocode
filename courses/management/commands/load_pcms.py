0fe25fd43ea1f2fb1da23774994523bbbfefb2e7
adilyas <adygamovilyas@gmail.com>
2024-05-02 22:09:36 +0300

    Fix pcms load

--- courses/management/commands/load_pcms.py
+++ courses/management/commands/load_pcms.py
@@ -4,6 +4,21 @@ from courses.lib.mongo import mongo
 from courses.models import Contest
 from courses.judges import pcms
 
+from courses.models import ContestStandingsHolder
+
+import json
+
+
+def upload_standings(contest, problems, runs_list):  # copy-paste from load_codeforces, refactor
+    if not mongo.upload_standings(contest, [problems, runs_list]):
+        try:
+            standings_holder = contest.standings_holder.get()
+        except:
+            standings_holder = ContestStandingsHolder(contest=contest)
+        standings_holder.problems = json.dumps(problems)
+        standings_holder.runs_list = json.dumps(runs_list)
+        standings_holder.save()
+
 
 class Command(BaseCommand):
     help = 'Loads data for pcms standings'
@@ -16,7 +31,8 @@ class Command(BaseCommand):
             try:
                 problems, runs_list = pcms.load_pcms_contest(contest, users)
                 if not mongo.upload_standings(contest, [problems, runs_list]):
-                    print("Can not upload standings to mongo")
+                    upload_standings(contest, problems, runs_list)
+                    # print("Can not upload standings to mongo")
             except Exception as e:
                 print("Can not update contest, error:", e)
             except:
